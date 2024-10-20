702bc5a2d98e8a95cb2e10f4025db1e29820b20e
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-10-20 19:24:20 +0900

    feat: develop equipment schema model

---
+++ apps/venuehub-api/src/schemas/Equipment.model.ts
@@ -0,0 +1,81 @@
+import { Schema } from 'mongoose';
+import { EquipmentStatus, EquipmentType } from '../libs/enums/equipment.enum';
+import { PropertyLocation, PropertyStatus, PropertyType } from '../libs/enums/property.enum';
+
+const EquipmentSchema = new Schema(
+	{
+		equipmentType: {
+			type: String,
+			enum: EquipmentType,
+			required: true,
+		},
+
+		equipmentStatus: {
+			type: String,
+			enum: EquipmentStatus,
+			default: EquipmentStatus.ACTIVE,
+		},
+
+		equipmentTitle: {
+			type: String,
+			required: true,
+		},
+
+		equipmentRentPrice: {
+			type: Number,
+			required: true,
+		},
+
+		equipmentViews: {
+			type: Number,
+			default: 0,
+		},
+
+		equipmentLikes: {
+			type: Number,
+			default: 0,
+		},
+
+		equipmentComments: {
+			type: Number,
+			default: 0,
+		},
+
+		equipmentRank: {
+			type: Number,
+			default: 0,
+		},
+
+		equipmentImages: {
+			type: [String],
+			required: true,
+		},
+
+		equipmentDesc: {
+			type: String,
+		},
+
+		memberId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'Member',
+		},
+
+		rentedAt: {
+			type: Date,
+		},
+
+		deletedAt: {
+			type: Date,
+		},
+
+		constructedAt: {
+			type: Date,
+		},
+	},
+	{ timestamps: true, collection: 'equipments' },
+);
+
+EquipmentSchema.index({ equipmentType: 1, equipmentTitle: 1, equipmentRentPrice: 1 }, { unique: true });
+
+export default EquipmentSchema;
